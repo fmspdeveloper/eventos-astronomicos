543e5f5fb2f094dd20e5c57cc8cf685102a6399c
FmDeveloper <103669513+fmspdeveloper@users.noreply.github.com>
2025-05-14 20:57:08 -0300

    update project add: Filter for datas (today and next two weeks for exibition the array

--- index.js
+++ index.js
@@ -2,10 +2,50 @@ import chuvasMeteoros from './data/chuva-de-meteoro.js';
 
 console.log("Chuvas de Meteoros")
 
-const nome = chuvasMeteoros.map (chuva => ({
-  nome: chuva.nome
-}));
 
-const todasChuvas = chuvasMeteoros.filter (chuva => ({}));
-console.log(nome);
-console.log(todasChuvas);
+const filtrarChuvasHoje = () => {
+  const hoje = new Date();
+
+  const dia = hoje.getDate().toString().padStart(2, '0');
+  const mes = (hoje.getMonth() + 1).toString().padStart(2, '0');
+
+  const dataFormatada = `${dia}/${mes}`;
+
+  return chuvasMeteoros.filter(chuva => {
+    const [inicioDia, inicioMes] = chuva.inicio.split('/');
+    const [fimDia, fimMes] = chuva.fim.split('/');
+
+    const inicioFormatado = `${inicioDia.padStart(2, '0')}/${inicioMes.padStart(2, '0')}`;
+    const fimFormatado = `${fimDia.padStart(2, '0')}/${fimMes.padStart(2, '0')}`;
+
+    return dataFormatada >= inicioFormatado && dataFormatada <= fimFormatado;
+
+  })
+};
+
+const filtrarProximosDoisMeses = () => {
+  const hoje = new Date();
+  const mesAtual = hoje.getMonth() + 1;
+  const anoAtual = hoje.getFullYear();
+
+  let mesFuturo = mesAtual + 2;
+  let anoFuturo = anoAtual;
+
+  if (mesFuturo > 12) {
+    mesFuturo -= 12;
+    anoFuturo -= 12;
+  }
+
+  return chuvasMeteoros.filter(chuva => {
+    const inicioMes = parseInt(chuva.inicio.split('/')[1]);
+    const fimMes = parseInt(chuva.fim.split('/')[1]);
+
+    return (
+      (inicioMes >= mesAtual && inicioMes <= mesFuturo) ||
+      (fimMes >= mesAtual && fimMes <= mesFuturo) ||
+      (inicioMes <= mesAtual && fimMes <= mesFuturo)
+    );
+  })
+}
+console.log("METEOROS HOJE " + filtrarChuvasHoje());
+console.log("METEOROS 2 MESES " + filtrarProximosDoisMeses())
